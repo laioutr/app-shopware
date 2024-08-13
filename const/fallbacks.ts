e123083be78d2b20802a4dd2fae30ed87218e927
Sebastian Langer <sl@scn.cx>
2024-08-13 17:59:15 +0200

    fix(shopware): workaround module-build by moving plugin into layer

--- const/fallbacks.ts
+++
@@ -1,4 +0,0 @@
-export const FALLBACK_IMAGE_URL = 'https://cdn.shopware.store/a/B/m/pPkDE/media/b1/48/d9/SW10069.jpg?ts=1596695192';
-
-export const FALLBACK_CURRENCY_CODE = 'EUR';
-export const FALLBACK_CURRENCY_DECIMAL_DIGITS = 2;
