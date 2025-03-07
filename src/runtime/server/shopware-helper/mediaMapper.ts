1fa9523a474746a14ce484d57bbd59f16e527b4d
Sebastian Langer <sebastian.langer@laioutr.com>
2025-03-07 19:29:57 +0100

    feat(ui): add proper media component implementation

--- src/runtime/server/shopware-helper/mediaMapper.ts
+++ src/runtime/server/shopware-helper/mediaMapper.ts
@@ -1,25 +1,40 @@
-import { Media } from '@laioutr-core/canonical-types';
+import { Media, MediaSourceImage, MediaSourceVideo } from '@laioutr-core/canonical-types';
 import { Schemas } from '../types/storeApiTypes';
 
 type SwMedia = Schemas['Media'];
 type SwMediaThumbnail = Schemas['MediaThumbnail'];
 
-export const mapMediaThumbnail = (thumbnail: SwMediaThumbnail) => ({
-  width: thumbnail.width ?? 0,
-  height: thumbnail.height ?? 0,
-  src: thumbnail.url,
-});
+export const mediaToSrc = (media: SwMedia) => {
+  const thumbnails = media.thumbnails ?? [];
+  const thumbnailSrc = thumbnails.map((thumb) => `${thumb.url} ${thumb.width}x${thumb.height}`);
+  const orgSrc = `${media.url} ${media.metaData?.width ?? 0}x${media.metaData?.height ?? 0}`;
+  return [...thumbnailSrc, orgSrc].join(', ');
+};
 
-export const mapMedia = (media: SwMedia): Media =>
+export const mapMediaSourceImage = (media: SwMedia): MediaSourceImage =>
   ({
-    type: media.mimeType?.startsWith('image/') ? 'image' : 'video',
+    provider: 'shopware',
+    width: media.metaData?.width ?? 0,
+    height: media.metaData?.height ?? 0,
+    src: mediaToSrc(media),
+    orgSrc: media.url as any,
+  }) satisfies MediaSourceImage;
+
+export const mapMediaSourceVideo = (media: SwMedia): MediaSourceVideo =>
+  ({
+    provider: 'shopware',
+    src: media.url as any,
+    width: media.metaData?.width ?? 0,
+    height: media.metaData?.height ?? 0,
+  }) satisfies MediaSourceVideo;
+
+export const mapMedia = (media: SwMedia): Media => {
+  const type = media.mimeType?.startsWith('image/') ? 'image' : 'video';
+  const source = type === 'image' ? mapMediaSourceImage(media) : mapMediaSourceVideo(media);
+
+  return {
+    type,
     alt: media.alt,
-    sources: [
-      {
-        provider: 'shopware',
-        width: media.metaData?.width ?? 0,
-        height: media.metaData?.height ?? 0,
-        src: (media.thumbnails?.map(mapMediaThumbnail) ?? []) as any,
-      },
-    ],
-  }) satisfies Media;
+    sources: [source],
+  } satisfies Media;
+};
