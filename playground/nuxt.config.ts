e24355d24fcf237fdc380a3e7841894f856e1ddf
Sebastian Langer <sebastian.langer@laioutr.com>
2025-05-12 17:00:32 +0200

    feat(orchestr): add devtools, type request context

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     srcModule,
     '@laioutr-core/frontend-core',
     '@laioutr-core/orchestr',
+    '@laioutr-core/orchestr-devtools',
   ],
   alias: {
     '@laioutr-app/shopware': fileURLToPath(new URL('../src', import.meta.url)),
@@ -14,4 +15,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   telemetry: false,
   compatibilityDate: '2024-11-11',
+  '@laioutr-app/shopware': {
+    endpoint: import.meta.env.SHOPWARE_DEMO_ENDPOINT,
+    accessToken: import.meta.env.SHOPWARE_DEMO_ACCESS_TOKEN,
+    adminEndpoint: import.meta.env.SHOPWARE_DEMO_ADMIN_ENDPOINT,
+    adminClientId: import.meta.env.SHOPWARE_DEMO_ADMIN_CLIENT_ID,
+    adminClientSecret: import.meta.env.SHOPWARE_DEMO_ADMIN_CLIENT_SECRET,
+  },
 });
