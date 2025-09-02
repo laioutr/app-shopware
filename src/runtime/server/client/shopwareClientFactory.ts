5df3f55c707f8be6aa7257250d4a45fcb288a57a
abdotaker608 <abdotaker607@gmail.com>
2025-09-02 11:34:36 +0300

    feat: add context awareness to storefront client for auth

--- src/runtime/server/client/shopwareClientFactory.ts
+++ src/runtime/server/client/shopwareClientFactory.ts
@@ -1,17 +1,29 @@
+import { OrchestrArgsBase } from '#orchestr/types/builder/Args';
 import { createAPIClient } from '@shopware/api-client';
-import { useRuntimeConfig } from '#imports';
-
+import { getCookie, setCookie, useRuntimeConfig } from '#imports';
 import type { StorefrontClient } from '../types/shopware';
 import type { operations } from '../types/storeApiTypes';
+import { CONTEXT_TOKEN_COOKIE } from '../const/cookieKeys';
 
 // Shopware client type is not exported
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore: ignore not portable type TS2742
-export const shopwareClientFactory = (): StorefrontClient => {
+export const shopwareClientFactory = (event: OrchestrArgsBase['event']): StorefrontClient => {
   const config = useRuntimeConfig()['@laioutr-app/shopware'];
+
   const client = createAPIClient<operations>({
     baseURL: config.endpoint,
     accessToken: config.accessToken,
+    contextToken: getCookie(event, CONTEXT_TOKEN_COOKIE),
+  });
+
+  client.hook('onContextChanged', (newContextToken) => {
+    setCookie(event, CONTEXT_TOKEN_COOKIE, newContextToken, {
+      maxAge: 365, // days
+      path: '/',
+      sameSite: 'lax',
+      secure: config.endpoint.startsWith('https://'),
+    });
   });
 
   client.defaultHeaders.apply({
