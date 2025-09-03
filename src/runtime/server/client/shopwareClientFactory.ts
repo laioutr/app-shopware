dee1be687625a7ca725c9dc54173f3e68117eaa2
abdotaker608 <abdotaker607@gmail.com>
2025-09-03 10:58:15 +0300

    fix: proper way of handling & storing persistent shopware context token

--- src/runtime/server/client/shopwareClientFactory.ts
+++ src/runtime/server/client/shopwareClientFactory.ts
@@ -5,25 +5,40 @@ import type { StorefrontClient } from '../types/shopware';
 import type { operations } from '../types/storeApiTypes';
 import { CONTEXT_TOKEN_COOKIE } from '../const/cookieKeys';
 
+// Global variable to use for storing context token upon triggering shopware api client `onContextChanged` hook
+// Can't set cookie directly at that time beause by that time response headers are already sent out
+const globalWithContextToken: typeof globalThis & { contextToken?: string } = globalThis;
+globalWithContextToken.contextToken = '';
+
 // Shopware client type is not exported
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore: ignore not portable type TS2742
 export const shopwareClientFactory = (event: OrchestrArgsBase['event']): StorefrontClient => {
   const config = useRuntimeConfig()['@laioutr-app/shopware'];
 
-  const client = createAPIClient<operations>({
-    baseURL: config.endpoint,
-    accessToken: config.accessToken,
-    contextToken: getCookie(event, CONTEXT_TOKEN_COOKIE),
-  });
+  const contextTokenCookie = getCookie(event, CONTEXT_TOKEN_COOKIE);
 
-  client.hook('onContextChanged', (newContextToken) => {
-    setCookie(event, CONTEXT_TOKEN_COOKIE, newContextToken, {
+  // Update global runetime variable from persistent cookie if found
+  if (contextTokenCookie) globalWithContextToken.contextToken = contextTokenCookie;
+
+  // Set persistent cookie if a context token exists
+  if (globalWithContextToken.contextToken) {
+    setCookie(event, CONTEXT_TOKEN_COOKIE, globalWithContextToken.contextToken, {
       maxAge: 365, // days
       path: '/',
       sameSite: 'lax',
       secure: config.endpoint.startsWith('https://'),
     });
+  }
+
+  const client = createAPIClient<operations>({
+    baseURL: config.endpoint,
+    accessToken: config.accessToken,
+    contextToken: globalWithContextToken.contextToken,
+  });
+
+  client.hook('onContextChanged', (newContextToken) => {
+    globalWithContextToken.contextToken = newContextToken;
   });
 
   client.defaultHeaders.apply({
