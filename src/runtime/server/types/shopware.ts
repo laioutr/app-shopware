f7e1d7e1531ee8ac81b343ae2a0e9f9621f1b84b
abdotaker608 <abdotaker607@gmail.com>
2025-09-15 10:26:41 +0300

    feat: implement product reviews creation & resolve

--- src/runtime/server/types/shopware.ts
+++ src/runtime/server/types/shopware.ts
@@ -2,6 +2,7 @@ import { createAPIClient } from '@shopware/api-client';
 import type { components, operations } from './storeApiTypes';
 
 export type ShopwareProduct = components['schemas']['Product'];
+export type ShopwareProductReview = components['schemas']['ProductReview'];
 export type ShopwareCategory = components['schemas']['Category'];
 export type ShopwareFilters = components['schemas']['Filters'];
 export type ShopwareManufacturer = components['schemas']['ProductManufacturer'];
