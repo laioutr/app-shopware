40b2504ad2b747bf60669cbe31061680f505d122
abdotaker608 <abdotaker607@gmail.com>
2025-10-01 22:02:44 +0300

    feat: cart implementation

---
+++ src/runtime/server/orchestr/cart/add-item.action.ts
@@ -0,0 +1,40 @@
+import { setCookie, useRuntimeConfig } from '#imports';
+import { CartAddItemsAction } from '@laioutr-core/canonical-types/ecommerce';
+import { CONTEXT_TOKEN_COOKIE } from '../../const/cookieKeys';
+import { defineShopwareAction } from '../../middleware/defineShopware';
+
+export default defineShopwareAction(CartAddItemsAction, async ({ event, context, input }) => {
+  const config = useRuntimeConfig()['@laioutr-app/shopware'];
+
+  const { storefrontClient } = context;
+
+  const products = input.filter((i) => i.type === 'product');
+
+  if (products.length > 0) {
+    const cart = await storefrontClient.invoke('addLineItem post /checkout/cart/line-item', {
+      body: {
+        items: products.map((product) => ({
+          children: {},
+          deliveryInformation: { apiAlias: 'cart_delivery_information' },
+          id: product.variantId ?? product.productId,
+          payload: {
+            type: product.type,
+            id: product.variantId ?? product.productId,
+          },
+          quantity: product.quantity,
+          states: ['is-physical'],
+          type: product.type,
+        })),
+      },
+    });
+
+    if (cart.data.token) {
+      setCookie(event, CONTEXT_TOKEN_COOKIE, cart.data.token, {
+        maxAge: 60 * 60 * 24 * 365, // days
+        path: '/',
+        sameSite: 'lax',
+        secure: config.endpoint.startsWith('https://'),
+      });
+    }
+  }
+});
